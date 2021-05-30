3299993223e2f54abeb29fe40f7022b51ba1a864
Alvaro <varito1@gmail.com>
2021-05-30 19:15:59 -0300

    Added moongose package. Updated mongodb connection. Created Media model

---
+++ models/media.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const MediaSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    poster: {
+      type: String,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    imdbID: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    collection: 'series-movies-watched',
+  }
+);
+
+export default mongoose.models.Media || mongoose.model('Media', MediaSchema);
