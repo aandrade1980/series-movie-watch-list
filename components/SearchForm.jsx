158ef9a8d5ec01ccea07b694b76ddf8c9f8c3f34
Alvaro <varito1@gmail.com>
2021-06-06 18:59:11 -0300

    Added a close icon in the search input

--- components/SearchForm.jsx
+++ components/SearchForm.jsx
@@ -1,25 +1,58 @@
-import { Box, Button, Flex, Input } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from '@chakra-ui/react';
 import { useForm } from 'react-hook-form';
-import { SearchIcon } from './Icons';
+
+import { CloseIcon, SearchIcon } from './Icons';
 
 export default function SearchForm({ isLoading, searchValue, setSearchValue }) {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset, watch } = useForm();
+
+  const searchInputValue = watch('inputValue');
 
   const onSubmit = ({ inputValue }) => {
     setSearchValue(inputValue);
     localStorage.setItem('searchValue', inputValue);
   };
 
+  const handleClearInput = () => {
+    reset({ inputValue: '' });
+    setSearchValue(null);
+    localStorage.setItem('searchValue', '');
+  };
+
   return (
     <Box mb={6}>
       <Flex as="form" onSubmit={handleSubmit(onSubmit)}>
         <Flex mr={2} flexDir="column">
-          <Input
-            placeholder="Star Wars"
-            type="text"
-            defaultValue={searchValue}
-            {...register('inputValue', { required: true })}
-          />
+          <InputGroup>
+            <Input
+              placeholder="Star Wars"
+              type="text"
+              defaultValue={searchValue}
+              autoComplete="off"
+              {...register('inputValue', { required: true })}
+            />
+            <InputRightElement
+              children={
+                searchInputValue ? (
+                  <CloseIcon
+                    color="red.600"
+                    h="1.4em"
+                    w="1.4em"
+                    _active={{ transform: 'scale(0.95)', color: 'red.400' }}
+                  />
+                ) : null
+              }
+              onClick={handleClearInput}
+              cursor="pointer"
+            />
+          </InputGroup>
         </Flex>
         <Button
           colorScheme="blue"
