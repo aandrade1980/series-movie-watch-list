9cbae9220d6ce5655a4da61818abec71b4ff43b7
Alvaro Andrade <varito1@gmail.com>
2022-12-14 09:47:52 -0300

    Added title to watched and movie pages. Updated dependencies. (#40)
    * :art: Added title to watched and movie pages. Updated dependencies.
    
    * Format code with prettier
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -1,5 +1,6 @@
-import { useMemo } from 'react';
-import { useRouter } from 'next/router';
+import { useMemo } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
 import {
   Box,
   Button,
@@ -7,22 +8,22 @@ import {
   Heading,
   HStack,
   Text,
-  Tooltip
-} from '@chakra-ui/react';
-import { useQueryClient, useMutation } from 'react-query';
-import { signIn, useSession } from 'next-auth/react';
+  Tooltip,
+} from "@chakra-ui/react";
+import { useQueryClient, useMutation } from "react-query";
+import { signIn, useSession } from "next-auth/react";
 
-import { CheckIcon, CheckedIcon } from '@/components/Icons';
-import Rating from '@/components/Rating';
-import Spinner from '@/components/Spinner';
-import MediaPoster from '@/components/MediaPoster';
+import { CheckIcon, CheckedIcon } from "@/components/Icons";
+import Rating from "@/components/Rating";
+import Spinner from "@/components/Spinner";
+import MediaPoster from "@/components/MediaPoster";
 
-import { useAllWatchedMediaByUser, useMediaById } from '@/hooks/media';
-import useToast from '@/hooks/toast';
+import { useAllWatchedMediaByUser, useMediaById } from "@/hooks/media";
+import useToast from "@/hooks/toast";
 
-import { removeMediaFromWatched, setMediaAsWatched } from '@/utils/fetch';
+import { removeMediaFromWatched, setMediaAsWatched } from "@/utils/fetch";
 
-import styles from '@/styles/Home.module.scss';
+import styles from "@/styles/Home.module.scss";
 
 const MediaPage = () => {
   const { data: session } = useSession();
@@ -33,7 +34,7 @@ const MediaPage = () => {
     isLoadingWatchedMedia,
     isErrorWatchedMedia,
     allWatchedMedia,
-    errorWatchedMedia
+    errorWatchedMedia,
   } = useAllWatchedMediaByUser(session?.user?.email);
 
   const queryClient = useQueryClient();
@@ -41,41 +42,45 @@ const MediaPage = () => {
   const toast = useToast();
 
   const watched = useMemo(
-    () => allWatchedMedia && allWatchedMedia.find(media => media.imdbID === id),
+    () =>
+      allWatchedMedia && allWatchedMedia.find((media) => media.imdbID === id),
     [allWatchedMedia, id]
   );
 
   const { mutate } = useMutation(
     watched ? removeMediaFromWatched : setMediaAsWatched,
     {
-      onMutate: async media => {
-        await queryClient.cancelQueries('allMediaWatched');
-        const previousMedia = queryClient.getQueryData('allMediaWatched');
+      onMutate: async (media) => {
+        await queryClient.cancelQueries("allMediaWatched");
+        const previousMedia = queryClient.getQueryData("allMediaWatched");
         const updatedMedia = [...previousMedia];
 
         watched
           ? queryClient.setQueryData(
-              'allMediaWatched',
+              "allMediaWatched",
               updatedMedia.filter(
-                eachValue => eachValue.imdbID !== media.imdbID
+                (eachValue) => eachValue.imdbID !== media.imdbID
               )
             )
-          : queryClient.setQueryData('allMediaWatched', old => [...old, media]);
+          : queryClient.setQueryData("allMediaWatched", (old) => [
+              ...old,
+              media,
+            ]);
 
         return { previousMedia };
       },
       onSuccess: () =>
         toast({
           title: `${media.Type} successfully set as ${
-            watched ? 'not watched' : 'watched'
+            watched ? "not watched" : "watched"
           }`,
-          status: 'success'
+          status: "success",
         }),
       onError: () =>
         toast({
-          title: 'Please try again later',
-          status: 'error'
-        })
+          title: "Please try again later",
+          status: "error",
+        }),
     }
   );
 
@@ -84,19 +89,19 @@ const MediaPage = () => {
   }
 
   if (isError) {
-    return console.error('Error getting media info: ', error);
+    return console.error("Error getting media info: ", error);
   }
 
   if (isErrorWatchedMedia) {
     return console.error(
-      'Error getting all media watched: ',
+      "Error getting all media watched: ",
       errorWatchedMedia
     );
   }
 
   const handleButtonClick = async () => {
     if (!session) {
-      return signIn('google');
+      return signIn("google");
     }
 
     const user = session.user.email;
@@ -104,107 +109,113 @@ const MediaPage = () => {
       ? { imdbID: media.imdbID, user }
       : {
           ...media,
-          user
+          user,
         };
     mutate(data);
   };
 
   return (
-    <Box
-      minH="calc(100vh - 4rem)"
-      background="radial-gradient(circle, rgba(72,85,83,1) 0%, rgba(55,50,55,1) 100%);"
-      pt={10}
-      color="#eeeeee"
-    >
-      <Flex mt={8} mx={24}>
-        <Box
-          height="375px"
-          width="250px"
-          mr={8}
-          style={{
-            boxShadow: '0 -2px 10px rgba(0, 0, 0, 1)',
-            borderRadius: '2%'
-          }}
-        >
-          <MediaPoster
-            poster={media.Poster}
-            title={media.Title}
-            height={375}
-            width={250}
-          />
-        </Box>
-        <Box minW="450px">
-          <Flex justifyContent="space-between" alignItems="center">
-            <Heading as="h3" size="lg" my={2} maxW="85%">
-              {media.Title}
-            </Heading>
-            <Tooltip
-              hasArrow
-              label={`Set as ${watched ? 'not watched' : 'watched'}`}
-              bg="gray.300"
-              color="black"
-              fontSize="sm"
-            >
-              <Button
-                variant="outline"
-                className={styles.watched_button}
-                onClick={handleButtonClick}
-              >
-                {watched ? (
-                  <CheckedIcon h={6} w={6} />
-                ) : (
-                  <CheckIcon h={6} w={6} />
-                )}
-              </Button>
-            </Tooltip>
-          </Flex>
-          <Text fontWeight="semibold" mb={2}>
-            {media.Year}
-          </Text>
-          <HStack>
-            <Text fontSize="sm" color="#919293" fontWeight="bold">
-              {media.Runtime}
-            </Text>
-            <Text fontSize="sm" color="#919293" fontWeight="bold">
-              {media.Rated}
-            </Text>
-            {media.Type === 'movie' && media.Ratings[1] && (
-              <Rating value={media.Ratings[1].Value} img="rotten.svg" />
-            )}
-            {media.Type === 'movie' && media.Ratings[2] && (
-              <Rating value={media.Ratings[2].Value} img="metacritic.svg" />
-            )}
-          </HStack>
-          <Box maxWidth="550px" mt={6}>
-            <Text>{media.Plot}</Text>
+    <>
+      <Head>
+        <title>{media.Title}</title>
+        <meta name="description" content="Movie info" />
+      </Head>
+      <Box
+        minH="calc(100vh - 4rem)"
+        background="radial-gradient(circle, rgba(72,85,83,1) 0%, rgba(55,50,55,1) 100%);"
+        pt={10}
+        color="#eeeeee"
+      >
+        <Flex mt={8} mx={24}>
+          <Box
+            height="375px"
+            width="250px"
+            mr={8}
+            style={{
+              boxShadow: "0 -2px 10px rgba(0, 0, 0, 1)",
+              borderRadius: "2%",
+            }}
+          >
+            <MediaPoster
+              poster={media.Poster}
+              title={media.Title}
+              height={375}
+              width={250}
+            />
           </Box>
-          <Flex mt={6} alignItems="center">
-            <Text
-              fontSize="sm"
-              textTransform="uppercase"
-              color="hsl(330,5%,60%)"
-              mr={6}
-              fontWeight="semibold"
-            >
-              Released
+          <Box minW="450px">
+            <Flex justifyContent="space-between" alignItems="center">
+              <Heading as="h3" size="lg" my={2} maxW="85%">
+                {media.Title}
+              </Heading>
+              <Tooltip
+                hasArrow
+                label={`Set as ${watched ? "not watched" : "watched"}`}
+                bg="gray.300"
+                color="black"
+                fontSize="sm"
+              >
+                <Button
+                  variant="outline"
+                  className={styles.watched_button}
+                  onClick={handleButtonClick}
+                >
+                  {watched ? (
+                    <CheckedIcon h={6} w={6} />
+                  ) : (
+                    <CheckIcon h={6} w={6} />
+                  )}
+                </Button>
+              </Tooltip>
+            </Flex>
+            <Text fontWeight="semibold" mb={2}>
+              {media.Year}
             </Text>
-            <Text fontSize="sm">{media.Released}</Text>
-          </Flex>
-          <Flex alignItems="center">
-            <Text
-              fontSize="sm"
-              textTransform="uppercase"
-              color="hsl(330,5%,60%)"
-              mr={12}
-              fontWeight="semibold"
-            >
-              Genre
-            </Text>
-            <Text fontSize="sm">{media.Genre}</Text>
-          </Flex>
-        </Box>
-      </Flex>
-    </Box>
+            <HStack>
+              <Text fontSize="sm" color="#919293" fontWeight="bold">
+                {media.Runtime}
+              </Text>
+              <Text fontSize="sm" color="#919293" fontWeight="bold">
+                {media.Rated}
+              </Text>
+              {media.Type === "movie" && media.Ratings[1] && (
+                <Rating value={media.Ratings[1].Value} img="rotten.svg" />
+              )}
+              {media.Type === "movie" && media.Ratings[2] && (
+                <Rating value={media.Ratings[2].Value} img="metacritic.svg" />
+              )}
+            </HStack>
+            <Box maxWidth="550px" mt={6}>
+              <Text>{media.Plot}</Text>
+            </Box>
+            <Flex mt={6} alignItems="center">
+              <Text
+                fontSize="sm"
+                textTransform="uppercase"
+                color="hsl(330,5%,60%)"
+                mr={6}
+                fontWeight="semibold"
+              >
+                Released
+              </Text>
+              <Text fontSize="sm">{media.Released}</Text>
+            </Flex>
+            <Flex alignItems="center">
+              <Text
+                fontSize="sm"
+                textTransform="uppercase"
+                color="hsl(330,5%,60%)"
+                mr={12}
+                fontWeight="semibold"
+              >
+                Genre
+              </Text>
+              <Text fontSize="sm">{media.Genre}</Text>
+            </Flex>
+          </Box>
+        </Flex>
+      </Box>
+    </>
   );
 };
 
--- pages/watched.jsx
+++ pages/watched.jsx
@@ -1,10 +1,11 @@
-import { useSession } from 'next-auth/react';
-import { Box } from '@chakra-ui/react';
+import { useSession } from "next-auth/react";
+import Head from "next/head";
+import { Box } from "@chakra-ui/react";
 
-import { useAllWatchedMediaByUser } from '@/hooks/media';
+import { useAllWatchedMediaByUser } from "@/hooks/media";
 
-import Spinner from '@/components/Spinner';
-import MovieList from '@/components/MovieList';
+import Spinner from "@/components/Spinner";
+import MovieList from "@/components/MovieList";
 
 export default function Watched() {
   const { data: session } = useSession();
@@ -13,7 +14,7 @@ export default function Watched() {
     isLoadingWatchedMedia,
     isErrorWatchedMedia,
     allWatchedMedia,
-    errorWatchedMedia
+    errorWatchedMedia,
   } = useAllWatchedMediaByUser(session?.user?.email);
 
   if (isLoadingWatchedMedia || !allWatchedMedia) {
@@ -21,21 +22,27 @@ export default function Watched() {
   }
 
   if (isErrorWatchedMedia) {
-    return console.error('Error getting media watched: ', errorWatchedMedia);
+    return console.error("Error getting media watched: ", errorWatchedMedia);
   }
 
   return (
-    <Box
-      minH="calc(100vh - 4rem)"
-      backgroundImage="linear-gradient(to top, #09203f 0%, #537895 100%)"
-      pt={10}
-      color="#eeeeee"
-    >
-      {allWatchedMedia && (
-        <div>
-          <MovieList movies={allWatchedMedia} />
-        </div>
-      )}
-    </Box>
+    <>
+      <Head>
+        <title>Watched movies</title>
+        <meta name="description" content="Watched movies" />
+      </Head>
+      <Box
+        minH="calc(100vh - 4rem)"
+        backgroundImage="linear-gradient(to top, #09203f 0%, #537895 100%)"
+        pt={10}
+        color="#eeeeee"
+      >
+        {allWatchedMedia && (
+          <div>
+            <MovieList movies={allWatchedMedia} />
+          </div>
+        )}
+      </Box>
+    </>
   );
 }
