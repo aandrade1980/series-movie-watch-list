269a7db871f03b6cbcea05d508024369e0384fad
Alvaro <varito1@gmail.com>
2022-12-14 09:56:24 -0300

    :art: Updated nextauth config.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,5 @@ module.exports = {
   images: {
     domains: ['m.media-amazon.com'],
     formats: ['image/avif', 'image/webp']
-  },
-  swcMinify: true
+  }
 };
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,14 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
-export default NextAuth({
-  secret: process.env.SECRET,
+export const authOptions = {
+  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-});
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+    // ...add more providers here
+  ]
+};
+export default NextAuth(authOptions);
